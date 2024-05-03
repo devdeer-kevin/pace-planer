1046126a6de2e690670056dc0e6c895fba18edcb
devdeer-kevin <kevin.heyland@devdeer.com>
2024-05-03 14:54:31 +0200

    Refactor pace calculation and finish time conversion in route.ts

--- src/app/api/v1/route.ts
+++ src/app/api/v1/route.ts
@@ -1,36 +1,46 @@
 import { NextResponse, NextRequest } from "next/server";
 
 export async function POST(req: NextRequest) {
+  // Get minutes and seconds from request body
   const body = await req.json();
   const minutes = body.minutes;
   const seconds = body.seconds;
 
+  // Format pace
   const formattedSeconds = Number(seconds);
-  const rawPace = `${minutes}.${formattedSeconds}`;
+  const decimalSeconds = Math.round((formattedSeconds / 60) * 100);
+  const rawPace = `${minutes}.${decimalSeconds}`;
   const pace = Number(rawPace);
 
-  const convertToTime = (time: number) => {
-    const hours = Math.floor(time);
-    const formattedHours = hours < 10 ? `0${hours}` : hours;
-    const minutes = Math.floor((time - hours) * 60);
-    const formattedMinutes = minutes < 10 ? `0${minutes}` : minutes;
-    const seconds = Math.round(((time - hours) * 60 - minutes) * 60);
-    const formattedSeconds = seconds < 10 ? `0${seconds}` : seconds;
-    return `${formattedHours}:${formattedMinutes}:${formattedSeconds}`;
-  };
-
+  // Define distances
   const marathonLength = 42.195;
   const halfMarathonLength = 21.0975;
   const tenKLength = 10;
   const fiveKLength = 5;
   const oneMileLength = 1.6;
 
+  // Calculate finish times for various distances
   const finishTimeMarathon = (marathonLength * pace) / 60;
   const finishTimeHalfMarathon = (halfMarathonLength * pace) / 60;
   const finishTime10K = (tenKLength * pace) / 60;
   const finishTime5K = (fiveKLength * pace) / 60;
   const finishTime1Mile = (oneMileLength * pace) / 60;
 
+  const convertToTime = (time: number) => {
+    // Function to convert decimal time to hh:mm:ss format
+    const pad = (num: number) => (num < 10 ? `0${num}` : num);
+
+    let hours = Math.floor(time);
+    let remainder = time - hours;
+    let minutes = Math.floor(remainder * 60);
+    remainder = remainder * 60 - minutes;
+    let seconds = Math.floor(remainder * 60);
+
+    //  Return formatted time
+    return `${pad(hours)}:${pad(minutes)}:${pad(seconds)}`;
+  };
+
+  // Return finish times for various distances
   return NextResponse.json({
     finishTimeMarathon: convertToTime(finishTimeMarathon),
     finishTimeHalfMarathon: convertToTime(finishTimeHalfMarathon),

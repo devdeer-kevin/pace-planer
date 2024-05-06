a6f1013f63a04b0096abea57384f68c5831e96f1
devdeer-kevin <kevin.heyland@devdeer.com>
2024-05-06 21:06:41 +0200

    Add first draft to calculate finish pace

--- src/app/api/v1/finishPace/route.ts
+++ src/app/api/v1/finishPace/route.ts
@@ -3,8 +3,8 @@ import { NextResponse, NextRequest } from "next/server";
 export async function POST(req: NextRequest) {
   // Get minutes and seconds from request body
   const body = await req.json();
-  const customDistanceName: string = body.distance;
-  const customDistance: number = body.customDistance;
+  // const customDistanceName: string = body.distance;
+  // const customDistance: number = body.customDistance;
   const hours: number = body.hours;
   const minutes: number = body.minutes;
 
@@ -20,25 +20,22 @@ export async function POST(req: NextRequest) {
     { name: "10 Kilometer", length: 10 },
     { name: "Halbmarathon", length: 21.0975 },
     { name: "Marathon", length: 42.195 },
-    { name: customDistanceName, length: customDistance },
+    // { name: customDistanceName, length: customDistance },
   ];
 
   const convertToTime = (time: number) => {
     // Function to convert decimal time to hh:mm:ss format
     const pad = (num: number) => (num < 10 ? `0${num}` : num);
 
-    let hours = Math.floor(time);
-    let remainder = time - hours;
-    let minutes = Math.floor(remainder * 60);
-    remainder = remainder * 60 - minutes;
-    let seconds = Math.floor(remainder * 60);
+    let minutes = Math.floor(time);
+    let seconds = Math.round((time - minutes) * 60);
 
     //  Return formatted time
-    return `${pad(hours)}:${pad(minutes)}:${pad(seconds)}`;
+    return `${pad(minutes)}:${pad(seconds)}`;
   };
 
   const racePace = distances.map((race) => {
-    const finishPace = (targetTime / race.length) * 60;
+    const finishPace = (targetTime * 60) / race.length;
     return {
       distance: race.name,
       finishTime: convertToTime(finishPace),

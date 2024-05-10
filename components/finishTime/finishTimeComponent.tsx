b06579a1a4ae8b3c8fc14df17b2136047a9d9fab
devdeer-kevin <kevin.heyland@devdeer.com>
2024-05-10 22:26:19 +0200

    refactor: Update finishTimeComponent to pad decimal seconds in pace calculation

--- components/finishTime/finishTimeComponent.tsx
+++ components/finishTime/finishTimeComponent.tsx
@@ -11,10 +11,10 @@ interface IRacePace {
 export default function FinishTimeComponent() {
   const [minutes, setMinutes] = useState("06");
   const [seconds, setSeconds] = useState("14");
-  const [inputDistance, setInputDistance] = useState("5");
+  const [customDistance, setCustomDistance] = useState("5");
   const [loading, setLoading] = useState(false);
   const [raceResult, setRaceResult] = useState<IRacePace[]>([]);
-  const [isActive, setIsActive] = useState("?k");
+  const [selectedDistance, setSelectedDistance] = useState("1.6k");
 
   const handleSubmit = (event: KeyboardEvent<HTMLInputElement>) => {
     if (event.key === "Enter" || event.key === "NumpadEnter") {
@@ -31,39 +31,48 @@ export default function FinishTimeComponent() {
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify({ minutes: minutes, seconds: seconds }),
+      body: JSON.stringify({
+        minutes: minutes,
+        seconds: seconds,
+      }),
     });
     const data = await response.json();
     setRaceResult(data);
     setLoading(false);
   };
-
   // Method to handle minutes input
   const minutesHandler = (event: ChangeEvent<HTMLInputElement>) => {
+    if (isNaN(Number(event.target.value))) {
+      return;
+    }
     setMinutes(event.target.value);
   };
 
   // Method to handle seconds input
   const secondsHandler = (event: ChangeEvent<HTMLInputElement>) => {
+    if (isNaN(Number(event.target.value))) {
+      return;
+    }
     setSeconds(event.target.value);
   };
 
   // Method to handle distance input
   const distanceHandler = (event: ChangeEvent<HTMLInputElement>) => {
-    setInputDistance(event.target.value);
+    setCustomDistance(event.target.value);
   };
 
   // Method to reset pace
   const resetPace = () => {
     setMinutes("06");
     setSeconds("14");
+    setCustomDistance("5");
     setRaceResult([]);
   };
 
   return (
     <>
-      <div className="flex flex-col items-center gap-4">
-        <div className="flex flex-col items-center gap-4 bg-slate-900 rounded-lg p-6 w-full sm:min-w-[400px]">
+      <div className="flex flex-col items-center gap-4 max-w-[400px]">
+        <div className="flex flex-col items-center gap-4 bg-slate-900 rounded-lg p-6 w-full ">
           <div>
             <p className="text-slate-50 font-bold text-center">
               Plane deine Zielzeiten
@@ -72,126 +81,149 @@ export default function FinishTimeComponent() {
           <div className="flex flex-row gap-2">
             <div>
               <button
-                onClick={() => setIsActive("1.6k")}
+                onClick={() => setSelectedDistance("1.6k")}
                 className={`${
-                  isActive === "1.6k" ? "bg-yellow-400" : "bg-slate-700"
-                } h-12 w-12 rounded-lg transition-all duration-150 ease-in-out`}
+                  selectedDistance === "1.6k" ? "bg-yellow-400" : "bg-slate-700"
+                } h-12 w-12 rounded-lg `}
               >
                 1.6k
               </button>
             </div>
             <div>
               <button
-                onClick={() => setIsActive("5k")}
+                onClick={() => setSelectedDistance("5k")}
                 className={`${
-                  isActive === "5k" ? "bg-yellow-400" : "bg-slate-700"
-                } h-12 w-12 rounded-lg transition-all duration-150 ease-in-out`}
+                  selectedDistance === "5k" ? "bg-yellow-400" : "bg-slate-700"
+                } h-12 w-12 rounded-lg `}
               >
                 5k
               </button>
             </div>
             <div>
               <button
-                onClick={() => setIsActive("10k")}
+                onClick={() => setSelectedDistance("10k")}
                 className={`${
-                  isActive === "10k" ? "bg-yellow-400" : "bg-slate-700"
-                } h-12 w-12 rounded-lg transition-all duration-150 ease-in-out`}
+                  selectedDistance === "10k" ? "bg-yellow-400" : "bg-slate-700"
+                } h-12 w-12 rounded-lg `}
               >
                 10k
               </button>
             </div>
             <div>
               <button
-                onClick={() => setIsActive("21k")}
+                onClick={() => setSelectedDistance("21k")}
                 className={`${
-                  isActive === "21k" ? "bg-yellow-400" : "bg-slate-700"
-                } h-12 w-12 rounded-lg transition-all duration-150 ease-in-out`}
+                  selectedDistance === "21k" ? "bg-yellow-400" : "bg-slate-700"
+                } h-12 w-12 rounded-lg `}
               >
                 21k
               </button>
             </div>
             <div>
               <button
-                onClick={() => setIsActive("42k")}
+                onClick={() => setSelectedDistance("42k")}
                 className={`${
-                  isActive === "42k" ? "bg-yellow-400" : "bg-slate-700"
-                } h-12 w-12 rounded-lg transition-all duration-150 ease-in-out`}
+                  selectedDistance === "42k" ? "bg-yellow-400" : "bg-slate-700"
+                } h-12 w-12 rounded-lg `}
               >
                 42k
               </button>
             </div>
-            <div>
+            {/* <div>
               <button
                 onClick={() => setIsActive("?k")}
                 className={`${
                   isActive === "?k" ? "bg-yellow-400" : "bg-slate-700"
-                } h-12 w-12 rounded-lg transition-all duration-150 ease-in-out`}
+                } h-12 w-12 rounded-lg `}
               >
                 ?k
               </button>
-            </div>
+            </div> */}
           </div>
         </div>
-        <div className="flex flex-col gap-4 bg-slate-900 rounded-lg p-6 w-full sm:min-w-[400px]">
-          <div className="flex flex-row justify-center gap-4 items-end">
-            <div className="flex flex-col items-center gap-2 text-left">
-              <label className="text-slate-500 text-xs h-4 font-light text-center">
-                Individuelle Distanz in km
-              </label>
-              <input
-                aria-label="Individuelle Distanz in km"
-                className="w-full accent-yellow-400 h-12 p-2 text-center bg-transparent rounded-md"
-                value={inputDistance}
-                onChange={distanceHandler}
-                onKeyDown={handleSubmit}
-                type="range"
-                min="0"
-                max="160"
-              />
-              <div className="text-yellow-400">{inputDistance} km</div>
+        {selectedDistance === "?k" && (
+          <div className="flex flex-col gap-4 bg-slate-900 rounded-lg p-6 w-full ">
+            <div className="flex flex-row justify-center gap-4 items-end">
+              <div className="flex flex-col items-center gap-2 text-left">
+                <label className="text-slate-500 text-xs h-4 font-light text-center">
+                  Individuelle Distanz in km
+                </label>
+                <input
+                  aria-label="Individuelle Distanz in km"
+                  className="w-full accent-yellow-400 h-12 p-2 text-center bg-transparent rounded-md"
+                  value={customDistance}
+                  onChange={distanceHandler}
+                  onKeyDown={handleSubmit}
+                  type="range"
+                  min="0"
+                  max="160"
+                />
+                <div className="text-yellow-400">{customDistance} km</div>
+              </div>
             </div>
           </div>
-        </div>
-        <div className="flex flex-col gap-4 bg-slate-900 rounded-lg p-6 w-full sm:min-w-[400px]">
+        )}
+        <div className="flex flex-col gap-4 bg-slate-900 rounded-lg p-6 w-full ">
           <div className="flex flex-row justify-center gap-4 items-end">
-            <div className="flex flex-row gap-2 text-left">
+            <div className="flex flex-row gap-4 text-left">
               <div className="flex flex-col gap-2">
                 <label className="text-slate-500 text-xs h-4 font-light">
                   Minuten
                 </label>
                 <input
+                  disabled={raceResult.length > 0}
                   aria-label="Minuten"
-                  className="text-center py-2 bg-transparent border border-1 border-slate-50 text-slate-50 rounded-md placeholder:text-slate-700"
+                  className="text-center py-2 bg-transparent border border-1 border-slate-50 text-slate-50 disabled:text-slate-500 disabled:border-slate-700 rounded-md placeholder:text-slate-700"
                   value={minutes}
                   onChange={minutesHandler}
                   onKeyDown={handleSubmit}
+                  maxLength={2}
                 />
               </div>
               <div className="flex flex-col gap-2">
                 <label className="text-slate-500 text-xs h-4 font-light">
-                  Minuten
+                  Sekunden
                 </label>
                 <input
+                  disabled={raceResult.length > 0}
                   aria-label="Sekunden"
-                  className="text-center py-2 bg-transparent border border-1 border-slate-50 text-slate-50 rounded-md placeholder:text-slate-700"
+                  className="text-center py-2 bg-transparent border border-1 border-slate-50 text-slate-50 disabled:text-slate-500 disabled:border-slate-700 rounded-md placeholder:text-slate-700"
                   value={seconds}
                   onChange={secondsHandler}
                   onKeyDown={handleSubmit}
+                  maxLength={2}
                 />
               </div>
             </div>
           </div>
         </div>
-        <div className="flex flex-col gap-4 bg-slate-900 rounded-lg px-6 pt-6 pb-8 w-full sm:min-w-[400px]">
+        <div className="flex flex-col gap-4 bg-slate-900 rounded-lg px-6 w-full ">
           <div className="flex flex-col items-center">
             {raceResult.length <= 0 ? (
-              <p className="text-slate-500 text-center break-words w-4/6 text-xs h-4 font-light">
+              <p className="text-slate-500 text-center break-words w-4/6 pt-6 pb-12 text-xs h-4 font-light">
                 Gib Distanz und Pace ein, um deine Zielzeit zu berechnen.
               </p>
             ) : (
-              <p className="text-slate-500 text-center break-words w-4/6 text-xs h-4 font-light">
-                Ergebnis
-              </p>
+              <div className="flex flex-col gap-5 pt-3 pb-3">
+                <p className="text-yellow-400 text-center text-2xl h-4 font-mono">
+                  {selectedDistance === "1.6k" && (
+                    <>{raceResult[0].finishTime}</>
+                  )}
+                  {selectedDistance === "5k" && <>{raceResult[1].finishTime}</>}
+                  {selectedDistance === "10k" && (
+                    <>{raceResult[2].finishTime}</>
+                  )}
+                  {selectedDistance === "21k" && (
+                    <>{raceResult[3].finishTime}</>
+                  )}
+                  {selectedDistance === "42k" && (
+                    <>{raceResult[4].finishTime}</>
+                  )}
+                </p>
+                <p className="text-yellow-400 font-mono text-xs text-center">
+                  Deine Pace
+                </p>
+              </div>
             )}
           </div>
         </div>
